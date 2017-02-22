4eae634fd3d0357b4923dfcb06363b83d5d23517
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-02-22 14:31:08 +0100

    Modified the MarkdownParserFactory to make it return the currently selected parser

--- MarkdownMonster/_Classes/MarkdownParser/MarkdownParserFactory.cs
+++ MarkdownMonster/_Classes/MarkdownParser/MarkdownParserFactory.cs
@@ -1,4 +1,7 @@
-﻿namespace MarkdownMonster
+﻿using MarkdownMonster.AddIns;
+using System;
+using System.Linq;
+namespace MarkdownMonster
 {
     /// <summary>
     /// Retrieves an instance of a markdown parser
@@ -12,8 +15,21 @@
         /// <param name="renderLinksAsExternal"></param>
         /// <returns></returns>
         public static IMarkdownParser GetParser(bool renderLinksAsExternal = false, bool usePragmaLines = false, bool forceLoad = false)
-        {            
-            return new MarkdownParserMarkdig(usePragmaLines: usePragmaLines, force: forceLoad);
+        {
+            try
+            {
+                MarkdownMonsterParserAddin parser = AddinManager.Current.ParserAddins
+                    .Where(p => p.Name.Equals(mmApp.Configuration.ParserName))
+                    .First();
+
+                return parser.GetParser();
+            }
+            // In case the parser is not found
+            // return the default parser
+            catch (InvalidOperationException)
+            {
+                return new MarkdownParserMarkdig(usePragmaLines: usePragmaLines, force: forceLoad);
+            }
         }
 
     }
